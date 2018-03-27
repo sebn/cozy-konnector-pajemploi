e28bfe0767c818410c6ba7c000d3ac30c695cd8f
Thiriot Christophe <doubleface@users.noreply.github.com>
2018-03-27 15:26:10 +0200

    Applied konnector checklist (#2)
    * feat: sentry setup
    
    * fix package.json repository
    
    * travis setup

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+// Force sentry DSN into environment variables
+// In the future, will be set by the stack
+process.env.SENTRY_DSN =
+  process.env.SENTRY_DSN ||
+  'https://0c0cb928ab40494085526d2af96fff5d:50c5609fbcea4bc5abeaa49c2eac8f3a@sentry.cozycloud.cc/30'
+
 const {
   BaseKonnector,
   log,
